d6a6e51e8b34d663581d8f1c73ce0fb9e57be1cd
Juan Arias <jmarias@stanford.edu>
2016-05-10 12:47:56 -0700

    fixing name

---
+++ age_demean.R
@@ -0,0 +1,11 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
+
+age <- demographics[4]
+
+demean_age <- age - sum(age)/length(age)
+
+write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
+
+print("done!")
