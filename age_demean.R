df365a553a22170a2e6bd69ef88dbf83123e6737
Juan Arias <jmarias@stanford.edu>
2016-05-10 13:06:12 -0700

    changing names

--- age_demean.R
+++ age_demean.R
@@ -2,9 +2,13 @@ args <- commandArgs(trailingOnly = TRUE)
 
 demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
 
-age <- demographics[4]
+age <- demographics$age
 
-demean_age <- age - sum(age)/length(age)
+mean_age <- sum(age)/length(age)
+stopifnot(mean_age < 100)
+stopifnot(mean_age > 10)
+
+demean_age <- age - mean_age
 
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
